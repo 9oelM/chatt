acb715424fe978425180dccdd6e113c05eb5e2e1
Joel M <hj923@hotmail.com>
2018-10-07 17:24:38 +0000

    add codes for instant messging

--- src/client/App.js
+++ src/client/App.js
@@ -1,23 +1,40 @@
 import React, { Component } from 'react';
 import './app.css';
 import ReactImage from './react.png';
-
+import io from 'socket.io-client';
+ 
 export default class App extends Component {
-  state = { username: null };
-
-  componentDidMount() {
-    fetch('/api/getUsername')
-      .then(res => res.json())
-      .then(user => this.setState({ username: user.username }));
+  
+  constructor(props){
+    super(props)
+    this.state = {
+      socket: io()
+    }
   }
-
+  
+  componentDidMount() {
+    const messages = document.getElementById("messages")
+    this.state.socket.on('chat message', function(msg){
+      const receivedMsg = document.createElement('li')
+      receivedMsg.textContent = msg
+      messages.appendChild(receivedMsg)
+    });
+  }
+  
+  sendMsg = (evt) => {
+    const form = document.getElementById('form')
+    const msg = document.getElementById('m')
+    this.state.socket.emit('chat message', msg.value);
+    msg.value = ""
+    evt.preventDefault()
+  }
+  
   render() {
-    const { username } = this.state;
-    return (
-      <div>
-        {username ? <h1>{`Hello ${username}`}</h1> : <h1>Loading.. please wait!</h1>}
-        <img src={ReactImage} alt="react" />
-      </div>
+    return (<React.Fragment>
+      <ul id="messages"></ul>
+    <form action="" id = "form" onSubmit = {this.sendMsg}>
+      <input id="m" autocomplete="off" /><button>Send</button>
+    </form></React.Fragment>
     );
   }
 }
--- src/server/index.js
+++ src/server/index.js
@@ -1,8 +1,22 @@
 const express = require('express');
 const os = require('os');
-
 const app = express();
+const http = require('http').Server(app);
 
 app.use(express.static('dist'));
 app.get('/api/getUsername', (req, res) => res.send({ username: os.userInfo().username }));
-app.listen(8080, () => console.log('Listening on port 8080!'));
+http.listen(8080, function(){
+  console.log('listening on *:8080');
+});
+const io = require('socket.io')(http);
+
+io.on('connection', function(socket){
+  console.log('a user connected');
+  socket.on('disconnect', function(){
+    console.log('user disconnected');
+  });
+  socket.on('chat message', function(msg){
+    console.log('message: ' + msg);
+    io.emit('chat message', msg);
+  });
+});
